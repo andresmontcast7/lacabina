f92ffb50617c6630f918833a2af6acb53be151f2
Andres Monterroso <andresmontcast@icloud.com>
2024-04-26 14:30:55 -0600

    feat: add mob blog

--- components/Header.jsx
+++ components/Header.jsx
@@ -55,6 +55,9 @@ const MobileMenu = ()=>{
       <NextLink href="/servicios">
       <MenuItem>Videos & Servicos </MenuItem>
       </NextLink>
+      <NextLink href="/blogs'" passHref>
+      <MenuItem  >Blogs</MenuItem>
+      </NextLink>
       <NextLink href="https://api.whatsapp.com/send?phone=50254446684&text=Hola%20Me%20gustaria%20mas%20%20Info%20%F0%9F%98%80">
       <MenuItem> Contactanos </MenuItem>
       </NextLink>
