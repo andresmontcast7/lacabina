be1e934d3a91e113030f566dd37b2423ae5a68b5
Andres Monterroso <andresmontcast@icloud.com>
2024-05-07 07:44:07 -0600

    feat: modify data

--- services/infoCardsData.js
+++ services/infoCardsData.js
@@ -4,19 +4,19 @@
         {
             title:"Bodas",
             title2:"Hello ",
-            desc:"Creamos una experiencia divertida, personalizada y de alta calidad en los mejores momentos que todos recordarán.",
+            desc:"Creamos una experiencia divertida con nuestras cabina de fotos en guatemala, personalizada y de alta calidad en los mejores momentos que todos recordarán.",
             imge:'/img/bodaEvento.png'
         },
         {
             title:"Fiestas",
             title2:"Miau ",
-            desc:"Nuestra experiencia de video 360 destaca en cualquier celebración. Nuestras opciones de redes sociales facilitan compartir instantáneamente fotos con sus invitados.",
+            desc:"Nuestra experiencia de video 360 destaca en cualquier celebración. Nuestras cabina de fotos guatemala te ofrecen opciones de redes sociales facilitan compartir instantáneamente fotos con sus invitados.",
             imge:"https://i.ibb.co/nwSqkvp/fiesta.png"
         },
         {
             title:"Corporativo",
             title2:"WOoof woof  ",
-            desc:"Ya sea que se trate de una activación de marca, un retiro corporativo o una feria comercial, podemos crear una activación con nuestro Video 360 con material echo por expertos en diseño digital que mejorará el conocimiento y la exposición de la marca.",
+            desc:"Ya sea que se trate de una activación de marca, un retiro corporativo o una feria comercial, podemos crear una activación con nuestra cabina de fotos guatemala, con material echo por expertos en diseño digital que mejorará el conocimiento y la exposición de la marca.",
             imge:"/img/eventoLac.png"
         }
     ]
